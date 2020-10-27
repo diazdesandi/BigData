33b6e4eb2c21a4798258e93d5ad0eed4c1017cc9
René <diaz.de.sandi.7@gmail.com>
2020-10-27 13:52:40 -0700

    Change locations, removing duplicates.
    Co-Authored-By: Cruz Ibarra <44855267+cmir22@users.noreply.github.com>

---
+++ Unit 1/Practices and homeworks/Practice 1/File4.scala
@@ -0,0 +1,8 @@
+// Using lice to extract "Luke"
+var msg = "Hola Luke yo soy tu padre!"
+msg.slice(5,9)
+
+/* 
+Difference between value and a variable
+Once assigned, value (val) cannot change while variable(var) offers the option to reassing the value.
+*/
