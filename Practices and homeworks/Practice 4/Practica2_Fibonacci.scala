f8f60af4d6296d330a6dcd20f48dde22bd1b2fdb
René <diaz.de.sandi.7@gmail.com>
2020-10-28 10:35:02 -0700

    Added Practice 3, exercise 1 and 2

--- Practices and homeworks/Practice 4/Practica2_Fibonacci.scala
+++ Practices and homeworks/Practice 4/Practica2_Fibonacci.scala
@@ -1,2 +1,10 @@
 // Fibonacci Explicit formula
-
+// Here we declarae each variable
+// and we asign a value
+var n=20
+// After this we start the for loop
+for(n <- Array.range(0,n+1)){
+var fibonnaci =(Math.pow(((1+Math.sqrt(5))/2),n)-Math.pow(((1-Math.sqrt(5))/2),n))/Math.sqrt(5)
+// Here we print each loop
+println(fibonnaci)
+}
