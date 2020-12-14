bff67bc8c4f4d3341d1c99e39a8bd3973343114d
René <diaz.de.sandi.7@gmail.com>
2020-12-14 02:20:32 -0800

    P5,P6,P7 updated, files added.
    Co-Authored-By: Cruz Ibarra <44855267+cmir22@users.noreply.github.com>

---
+++ Practices and homeworks/Practice 7/practice7.scala
@@ -0,0 +1,38 @@
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.sql.SparkSession
+
+object MultilayerPerceptronClassifierExample {
+
+  def main(): Unit = {
+
+    val spark = SparkSession
+      .builder
+      .appName("MultilayerPerceptronClassifierExample")
+      .getOrCreate()
+
+    val data = spark.read.format("libsvm")
+      .load("sample_multiclass_classification_data.txt")
+
+    val splits = data.randomSplit(Array(0.6, 0.4), seed = 1234L)
+    val train = splits(0)
+    val test = splits(1)
+
+    val trainer = new MultilayerPerceptronClassifier()
+      .setLayers(layers)
+      .setBlockSize(128)
+      .setSeed(1234L)
+      .setMaxIter(100)
+
+    val model = trainer.fit(train)
+
+    val result = model.transform(test)
+    val predictionAndLabels = result.select("prediction", "label")
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setMetricName("accuracy")
+
+    println(s"Test set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
+
+    spark.stop()
+  }
+}
