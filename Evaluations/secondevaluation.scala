cabdf3c773473d3bb588090d186e54a673e2bf0e
René <diaz.de.sandi.7@gmail.com>
2020-12-14 02:21:25 -0800

    Second evaluation and P4 updated
    Co-Authored-By: Cruz Ibarra <44855267+cmir22@users.noreply.github.com>

--- Evaluations/secondevaluation.scala
+++ Evaluations/secondevaluation.scala
@@ -1,7 +1,30 @@
+// Import Apache Spark session
 import org.apache.spark.sql.SparkSession
 
 val spark = SparkSession.builder().getOrCreate()
 
-val dataf = spark.read.option("header", "true").option("inferSchema","true")csv("iris.csv")
+// 1. a) Spark Mllib import.
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+
+val dataf = spark.read.option("header", "true").option("inferSchema","true")csv("/home/rene/Documents/GitHub/BigData/Evaluations/iris.csv")
 
 var df = dataf.distinct()
+
+// 2. Column names
+df.columns
+
+// 3. Schema
+df.printSchema()
+
+// 4. Print the first 5 columns.
+df.head (5)
+
+// 5. Use describe method.
+df.describe().show()
+
+// 6. Categorical data
+
+// 7. Classification model
+
+// 8. Print classification model results
