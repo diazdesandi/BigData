6a3e21c6ee21d17a670edd91051b58f3712981cc
Cruz Ibarra <jackzelda_vicio@hotmail.com>
2020-12-14 18:24:03 -0800

    Added steps

--- Evaluations/secondevaluation.scala
+++ Evaluations/secondevaluation.scala
@@ -1,15 +1,18 @@
 // Import Apache Spark session
 import org.apache.spark.sql.SparkSession
-
+// We most import this to use the indexed and assembler
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.feature.VectorAssembler
 val spark = SparkSession.builder().getOrCreate()
 
 // 1. a) Spark Mllib import.
 import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 
-val dataf = spark.read.option("header", "true").option("inferSchema","true")csv("/home/rene/Documents/GitHub/BigData/Evaluations/iris.csv")
+var dataFrame = spark.read.option("header", "true").option("inferSchema","true")csv("iris.csv")
 
-var df = dataf.distinct()
+// Here we create the district by the dataframe create previsuly
+val df = dataFrame.distinct();
 
 // 2. Column names
 df.columns
@@ -24,7 +27,33 @@ df.head (5)
 df.describe().show()
 
 // 6. Categorical data
+val tagIndexer = new StringIndexer().setInputCol("species").setOutputCol("indexedLabel").fit(df)
+val index = tagIndexer.transform(df).drop("species").withColumnRenamed("indexedLabel", "label")
+index.describe().show()
 
 // 7. Classification model
 
+// First we need to import the vectorassembler to able join or assemble the data
+val assem = new VectorAssembler().setInputCols(Array("sepal_length","sepal_width","petal_length","petal_width")).setOutputCol("features");
+// After this we have to index it
+val  features = assem.transform(index);
+// And separate the data to manage more easily
+val split = features.randomSplit(Array(0.6, 0.4), seed = 1234L);
+val train = split(0);
+val test = split(1);
+// We input some random numbers
+val layers = Array[Int](4, 5, 4, 3);
+// Here we create the object to train the information by data
+val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setBlockSize(128).setSeed(1234L).setMaxIter(100);
+// Now we can create the model trained
+val model = trainer.fit(train);
+//Here we get the result
+val result = model.transform(test);
+val predictionAndLabels = result.select("prediction", "label");
+// To know the rate of prediction we invoque the next line
+predictionAndLabels.show;
+
 // 8. Print classification model results
+val evaluator = new MulticlassClassificationEvaluator().setMetricName("accuracy")
+// Finnaly we print the conclution representated on numbers
+println(s"Test set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
